036bda3681a87afd8975594efcf5f8255bfef150
yucarl77 <yucarl77@foxmail.com>
2022-07-24 00:32:24 +0800

    feat: auto-clean dist

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,6 +1,7 @@
 import typescript from "@rollup/plugin-typescript";
 import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
+import del from "rollup-plugin-delete";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
 
@@ -13,7 +14,11 @@ const plugins = [
   nodeResolve(),
 ];
 
-if (isDev) plugins.push(terser());
+if (isDev) {
+  plugins.push(terser(), del({ targets: "dist/*", runOnce: true }));
+} else {
+  plugins.push(terser(), del({ targets: "dist/*" }));
+}
 
 export default {
   input: "./src/main.ts",
