27e93fa582eea67ac4b8e79a06a5590a00be7bf9
dockfries <yucarl77@foxmail.com>
2025-10-13 20:46:54 +0800

    feat: replace pino logger with winston, update pkg scripts

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -1,31 +1,34 @@
-import { join } from "path";
-import pino from "pino";
+import process from "node:process";
+import winston from "winston";
 
-export const logger = pino({
-  transport: {
-    targets: [
-      {
-        target: "pino-pretty",
-        options: {
-          translateTime: "SYS:standard",
-          ignore: "pid,hostname",
-        },
-      },
-      {
-        target: "pino-roll",
-        options: {
-          file: join("logs", "log"),
-          mkdir: true,
-          frequency: "daily",
-          limit: {
-            count: 3,
-          },
-          dateFormat: "yyyy-MM-dd",
-        },
-      },
-    ],
-  },
+export const logger = winston.createLogger({
+  format: winston.format.combine(
+    winston.format.timestamp({
+      format: "YYYY-MM-DD HH:mm:ss",
+    }),
+    winston.format.json()
+  ),
+  transports: [
+    new winston.transports.File({
+      dirname: "logs",
+      filename: "log.txt",
+      maxsize: 1024 * 1024 * 5,
+    }),
+  ],
 });
+
+if (process.env.NODE_ENV !== "production") {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.printf((info) => {
+          return `[${info.timestamp}] ${info.level}: ${info.message}`;
+        })
+      ),
+    })
+  );
+}
 
 process.on("uncaughtException", (err) => {
   logger.error(err);
