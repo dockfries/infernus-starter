5e8454915344747c455601c4adbc19effa541046
dockfries <yucarl77@foxmail.com>
2025-10-15 05:05:26 +0800

    feat: esm, no more polyfill for stuck process

--- src/polyfill.js
+++
@@ -1,6 +0,0 @@
-import __module from "module";
-const __load = __module._load;
-__module._load = function (request) {
-  if (request === "process" || request === "node:process") return global.process;
-  return __load.apply(this, arguments);
-};
